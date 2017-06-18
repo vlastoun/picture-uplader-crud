36c69f82fb372cdefb5e71cd82c28732b96ddb72
Unknown <vlastoun@gmail.com>
2017-06-18 18:07:28 +0200

    upload to cloudinary

--- app/components/AdminDashboard/index.js
+++ app/components/AdminDashboard/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AdminHeader from 'components/AdminHeader';
 import Form from 'components/Form';
+import Script from 'react-load-script';
+import { CLOUDI_PRESET, CLOUDI_NAME, API_KEY } from './../../../keys';
 
+const SCRIPT_URL = '//widget.cloudinary.com/global/all.js';
 /* eslint-disable react/prefer-stateless-function */
 class DashBoard extends React.Component {
+  uploadWidget() {
+    cloudinary.openUploadWidget({//eslint-disable-line
+      cloud_name: CLOUDI_NAME,
+      upload_preset: CLOUDI_PRESET,
+      api_key: API_KEY,
+    },
+      (error, result) => {
+          console.log(result);//eslint-disable-line
+      });
+  }
   render() {
     return (
       <div>
         <AdminHeader logout={this.props.logout} />
         <Form mainPage>
+          <Script url={SCRIPT_URL} />
+          <button onClick={this.uploadWidget}>Upload image</button>
         </Form>
       </div>
     );

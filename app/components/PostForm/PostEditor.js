3cac9d6550957ec1d4865169eb1d700cbd696940
Vlastik <vlastoun@gmail.com>
2017-06-27 09:26:26 +0200

    setup of draft js

--- app/components/PostForm/PostEditor.js
+++ app/components/PostForm/PostEditor.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import {Editor, EditorState} from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import { ContentState, EditorState, RichUtils } from 'draft-js'; //eslint-disable-line
+
 
 class PostEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = this.onChange.bind(this);
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = (editorState) => this.setState({ editorState });
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
   }
 
-  onChange(editorState) {
-    this.setState({editorState});
-    console.log(editorState.toJS());
+
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
   }
+
   render() {
-    return <Editor editorState={this.state.editorState} onChange={this.onChange} />;
+    return (
+      <div>
+        <Editor
+          editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}
+        />
+      </div>
+    );
   }
 }
 
 export default PostEditor;
-
